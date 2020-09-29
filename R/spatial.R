b46fb19d87f4326e8827cbca8c4c8aa713003d37
Ben Best <ben@ecoquants.com>
2020-09-29 15:48:12 -0700

    ∆ docs

--- R/spatial.R
+++ R/spatial.R
@@ -1,192 +1,3 @@
-#' Get NMS polygon
-#'
-#' given NMS code (see
-#' \url{https://sanctuaries.noaa.gov/library/imast_gis.html}), download and
-#' extract zip, cache shapefile or read existing shapefile
-#'
-#' @param nms code for national marine sanctuary
-#' @param dir_shp directory to store cached shapefile
-#'
-#' @return sf object
-#' @export
-#'
-#' @examples
-
-#' Generate statistics for raster data within NMS polygon
-#'
-#' @param sanctuary_code code for national marine sanctuary
-#' @param erddap_id the name of the satellite data set to be pulled from erddap servers
-#' @param erddap_fld the parameter to be pulled from the data set
-#' @param year year of requested data in integer form
-#' @param month month of requested data in integer form
-#' @param stats the statistics to be generated for the data set
-#'
-#' @return statistics
-#' @export
-#' @import glue sf magrittr dplyr here
-#'
-#' @examples
-# This function gets the polygons for a National Marine Sanctuary
-get_nms_polygons <- function(nms){
-  # nms_shp <- here::here(glue::glue("data/shp/cinms_py.shp"))
-  location<-here::here()
-  start_point <- nchar(location) - nchar(nms) +1
-  if (substr(location, start_point, nchar(location)) == nms){
-    sanctuary_in_path = TRUE
-  } else {
-    sanctuary_in_path = FALSE
-  }
-
-  if (sanctuary_in_path == TRUE) {
-   nms_shp <- here::here(glue::glue("data/shp/{nms}_py.shp"))
-  } else {
-    nms_shp <- paste0(location, "/", nms, "/data/shp/", nms, "_py.shp")
-  }
-
-  # download if needed
-  if (!file.exists(nms_shp)){
-
-    nms_url <- glue::glue("https://sanctuaries.noaa.gov/library/imast/{nms}_py2.zip")
-
-    if (sanctuary_in_path == TRUE) {
-      nms_zip <- here::here(glue::glue("data/{nms}.zip"))
-      shp_dir <- here::here("data/shp")
-    } else {
-      nms_zip <- paste0(location, "/", nms, "/data/", nms, ".zip")
-      shp_dir <-paste0(location, "/", nms, "/data/shp")
-    }
-
-    download.file(nms_url, nms_zip)
-    unzip(nms_zip, exdir = shp_dir)
-    file_delete(nms_zip)
-  }
-  # read and convert to standard geographic projection
-  sf::read_sf(nms_shp) %>%
-    sf::st_transform(4326)
-}
-
-#' Extract ERDDAP statistics from polygon by year-month
-#'
-#' Extract satellite data in an ERDDAP dataset from national marine sanctuary polygon for a given year and month.
-#'
-#' @param sanctuary_code sanctuary code based on prefix to \href{https://sanctuaries.noaa.gov/library/imast_gis.html}{Sanctuary GIS files}, e.g. \code{"cinms"} for Channel Islands Marine Sanctuary
-#' @param erddap_id dataset ID of ERDDAP dataset (see \href{https://coastwatch.pfeg.noaa.gov/erddap/index.html}{coastwatch.pfeg.noaa.gov/erddap}), e.g. \code{"jplMURSST41mday"} for Multi-scale Ultra-high Resolution SST Analysis
-#' @param erddap_fld variable of ERDDAP dataset to extract, e.g. \code{"sst"}
-#' @param year 4-digit year
-#' @param month integer month (1-12)
-#' @param stats statistics to be generated for the data set \code{c("mean", "sd")}, to calculate mean and standard deviation)
-#'
-#' @return a list of values by statistic
-#' @export
-#' @import glue rerddap
-#'
-#' @examples
-ply2erddap <- function (sanctuary_code, erddap_id, erddap_fld, year, month, stats) {
-
-  # check inputs
-  stopifnot(all(is.numeric(year), is.numeric(month)))
-
-  # Get the polygons for the sanctuary.
-  sanctuary_ply <-   sf::as_Spatial(sf::st_union(nms4r::get_nms_polygons(sanctuary_code)))
-
-  # set the x and y limits of the raster to be pulled based upon the sanctuary polygons
-  bb <- sf::st_bbox(sanctuary_ply)
-
-  # TODO: deal with wrapping around dateline
-  # https://github.com/rstudio/leaflet/issues/225#issuecomment-347721709
-
-  # The dates to be considered (note that dates are handled differently when pulling different datasets)
-  m_beg   <- lubridate::ymd(glue::glue("{year}-{month}-01"))
-  m_end   <- m_beg + lubridate::days(lubridate::days_in_month(m_beg)) - lubridate::days(1)
-
-  # pull data from errdap server, with the process handled differently based upon the dataset - the value of erddap_id
-  # (as the datasets are not structured identically)
-
-  # set desired date range
-  m_dates <- c(m_beg, m_end)
-
-  nc <- try(
-    rerddap::griddap(
-      rerddap::info(erddap_id),
-      url = "https://coastwatch.pfeg.noaa.gov/erddap/",
-      time = m_dates,
-      #time = c("2013-01-01", "2013-06-01"),
-      #time = "2013-04-01",
-      latitude = c(bb$ymin, bb$ymax), longitude = c(bb$xmax, bb$xmin),
-      fields = erddap_fld, fmt = 'nc'))
-  if (class(nc) == "try-error"){
-    stats_na <- setNames(rep(NA, length(stats)), stats) %>% as.list()
-    return(stats_na)
-  }
-
-  if (erddap_id == "nesdisVHNSQchlaMonthly") { # pulling monthly chlorophyll data
-    # TODO: delete this chunk since this dataset now seems gone or renamed?, per https://coastwatch.pfeg.noaa.gov/erddap/griddap/nesdisVHNSQchlaMonthly.graph
-    ylim <- range(nc$data$lat, na.rm = TRUE)
-    xlim <- range(nc$data$lon, na.rm = TRUE)
-    ext <- raster::extent(xlim[1], xlim[2], ylim[1], ylim[2])
-
-    #create raster
-    d <- dplyr::arrange(nc$data, desc(nc$data$lat), nc$data$lon)
-    r <- raster::raster(nrows = length(unique(nc$data$lat)), ncols = length(unique(nc$data$lon)),
-                        ext = ext, vals = d[,erddap_fld])
-  } else { # if errdap_id calls any other dataset, stop everything as who knows how this other dataset is structured
-    # stop("Error in erddap_id: this function only currently knows how to handle the datasets jplMURSST41mday and nesdisVHNSQchlaMonthly")
-    r <- raster::raster(nc$summary$filename)
-  }
-
-  # The following get_stat function extracts a statistical value (eg. mean or standard deviation) from the raster
-  # cells remaining after being overlaid with the sanctuary polygons
-
-  get_stat <- function(stat){
-    fxn <- get(stat)
-    raster::extract(
-      r, sanctuary_ply, layer = 1,
-      method = "simple", na.rm=TRUE, fun = fxn)
-  }
-
-  # Let's run the function get_stat for every statistic asked for by the parameter value stats - this is the overall function output
-  sapply(stats, get_stat)
-}
-
-#' Generate statistics for last month for NMS polygon
-#'
-#' @param wrapper_sanctuary_code code for national marine sanctuary
-#' @param wrapper_erddap_id the name of the satellite data set to be pulled from erddap servers
-#' @param wrapper_erddap_fld the parameter to be pulled from the data set
-#' @param wrapper_stats the statistics to be generated for the data set
-#'
-#' @return nothing
-#' @export
-#'
-#' @examples
-generate_latest_SST<- function(wrapper_sanctuary_code, wrapper_erddap_id, wrapper_erddap_fld, wrapper_stats){
-  # this function generates the SST statistics for the latest month of available data. It (hopefully) is
-  # run near the beginning of the month and generates the SST statistics for the month before that.
-
-  # a function to generate first day of current month
-  som <- function(x) {
-    as.Date(format(x, "%Y-%m-01"))
-  }
-
-  # calculate the month and year of last month
-  last_month <- som(som(Sys.Date()) - 1)
-  year <- as.numeric(substr(last_month, 1, 4))
-  month <- as.numeric(substr(last_month, 6, 7))
-
-  # The date range to be considered
-  m_beg   <- lubridate::ymd(glue::glue("{year}-{month}-01"))
-  m_end   <- m_beg + lubridate::days(lubridate::days_in_month(m_beg)) - lubridate::days(1)
-  m_dates <- c(m_beg, m_end)
-
-  # write to this file
-  SST_file <- paste0(here::here("data/oceano/"),"avg-sst_", wrapper_sanctuary_code, ".csv")
-
-  # generate requested statistics using the ply2erddap function and then append the requested data to
-  # the csv file that has the data
-  write_out = ply2erddap(sanctuary_code = wrapper_sanctuary_code, erddap_id = wrapper_erddap_id, wrapper_erddap_fld, year = year, month = month, stats = wrapper_stats)
-  write(paste0(year, "-" , month, "-16," , round(write_out[1], 5), "," , round(write_out[2], 5)), file = SST_file, append = TRUE)
-  return(invisible())
-}
 
 #' Generate statistics for any missing months for NMS Sanctuary
 #'
@@ -298,6 +109,7 @@ calculate_statistics <-function(sanctuary, erddap_id, metric, csv_file) {
   return(invisible())
 }
 
+
 #' get_box
 #'
 #' @param lon
@@ -332,6 +144,58 @@ get_dates <- function(info){
     as.POSIXct(origin = "1970-01-01", tz = "GMT")
 }
 
+#' Get NMS polygon
+#'
+#' given NMS code (see
+#' \url{https://sanctuaries.noaa.gov/library/imast_gis.html}), download and
+#' extract zip, cache shapefile or read existing shapefile
+#'
+#' @param nms code for national marine sanctuary
+#' @param dir_shp directory to store cached shapefile
+#'
+#' @return sf object
+#' @export
+#'
+#' @examples
+get_nms_polygon <- function(nms){
+  # nms_shp <- here::here(glue::glue("data/shp/cinms_py.shp"))
+  location<-here::here()
+  start_point <- nchar(location) - nchar(nms) +1
+  if (substr(location, start_point, nchar(location)) == nms){
+    sanctuary_in_path = TRUE
+  } else {
+    sanctuary_in_path = FALSE
+  }
+
+  if (sanctuary_in_path == TRUE) {
+    nms_shp <- here::here(glue::glue("data/shp/{nms}_py.shp"))
+  } else {
+    nms_shp <- paste0(location, "/", nms, "/data/shp/", nms, "_py.shp")
+  }
+
+  # download if needed
+  if (!file.exists(nms_shp)){
+
+    nms_url <- glue::glue("https://sanctuaries.noaa.gov/library/imast/{nms}_py2.zip")
+
+    if (sanctuary_in_path == TRUE) {
+      nms_zip <- here::here(glue::glue("data/{nms}.zip"))
+      shp_dir <- here::here("data/shp")
+    } else {
+      nms_zip <- paste0(location, "/", nms, "/data/", nms, ".zip")
+      shp_dir <-paste0(location, "/", nms, "/data/shp")
+    }
+
+    download.file(nms_url, nms_zip)
+    unzip(nms_zip, exdir = shp_dir)
+    file_delete(nms_zip)
+  }
+  # read and convert to standard geographic projection
+  sf::read_sf(nms_shp) %>%
+    sf::st_transform(4326)
+}
+
+
 #' get_raster
 #'
 #' @param info
@@ -353,25 +217,6 @@ get_raster <- function(info, lon, lat, date="last", field="sst"){
 
 }
 
-#' get_raster_2
-#'
-#' @param info
-#' @param lon
-#' @param lat
-#' @param date
-#' @param field
-#'
-#' @return
-#' @export
-#'
-#' @examples
-get_raster_2 <- function(info, lon, lat, date="last", field="chlor_a"){
-  g_2 <- griddap(
-    info, longitude = lon, latitude = lat,
-    time = c(date, date), fields = field)
-  grid_to_raster(g_2, "chlor_a") %>%
-    leaflet::projectRasterForLeaflet(method="ngb")
-}
 
 #' get_timeseries
 #'
@@ -490,44 +335,17 @@ map_raster <- function(r, site_lon, site_lat, site_label, title){
       options = layersControlOptions(collapsed = T))
 }
 
-#' map_raster_2
-#'
-#' @param r
-#' @param site_lon
-#' @param site_lat
-#' @param site_label
-#' @param title
-#'
-#' @return
-#' @export
-#'
-#' @examples
-map_raster_2 <- function(r, site_lon, site_lat, site_label, title){
-  pal <- colorNumeric(colors$temperature, values(r), na.color = "transparent")
-
-  leaflet() %>%
-    addProviderTiles(providers$Esri.OceanBasemap, group="Color") %>%
-    addProviderTiles(providers$Stamen.TonerLite, group="B&W") %>%
-    #addProviderTiles(providers$Stamen.TonerLabels) %>%
-    addRasterImage(r, colors = pal, opacity = 0.8, project=F, group="CHL") %>%
-    addMarkers(lng = site_lon, lat = site_lat, label = site_label) %>%
-    addLegend(pal = pal, values = values(r), title = title, position="bottomright") %>%
-    addLayersControl(
-      baseGroups = c("Color", "B&W"),
-      overlayGroups = c("CHL"),
-      options = layersControlOptions(collapsed = T))
-}
-
 #' plot_metric_timeseries
 #'
 #' @param csv
 #' @param metric
+#' @param ... additional parameters to pass to \link[dygraphs]{dygraph}
 #'
 #' @return
 #' @export
 #'
 #' @examples
-plot_metric_timeseries <- function(csv, metric){
+plot_metric_timeseries <- function(csv, metric, ...){
   # The purpose of this function is to generate figures showing the sea surface temperature time series
   # for a Sanctuary (displaying both avg and standard deviation of temp). The function has two parameters: 1)
   # csv: which is the path name for the csv data file to be plotted and 2) metric: which is the type of data
@@ -545,11 +363,19 @@ plot_metric_timeseries <- function(csv, metric){
 
   # create the figure
   if (metric == "sst"){ # plotting sea surface temperature
-    dygraph(history, main = "Sea Surface Temperature", xlab = "Date", ylab = "Temperature (°C)")%>%
+    dygraph(
+      history,
+      main = "Sea Surface Temperature",
+      xlab = "Date", ylab = "Temperature (°C)",
+      ...)%>%
       dySeries(c("lower", "avg_value", "upper"), label = "Temperature (°C)", color = "Red")%>%
       dyRangeSelector()
   } else if (metric == "chl") { # plotting chlorophyll
-    dygraph(history, main = "Chlorophyll Concentration", xlab = "Date", ylab = "Chlorophyll Concentration, OC3 Algorithm (mg/m<sup>3</sup>)")%>%
+    dygraph(
+      history,
+      main = "Chlorophyll Concentration",
+      xlab = "Date", ylab = "Chlorophyll Concentration, OC3 Algorithm (mg/m<sup>3</sup>)",
+      ...)%>%
       dySeries(c("lower", "avg_value", "upper"), label = "Chlorophyll concentration", color = "Green")%>%
       dyRangeSelector()
   } else { # if any other metric is called, stop everything
@@ -580,4 +406,89 @@ plot_timeseries <- function(d, title="SST", color="red", dyRangeSelector=T, ...)
       dyRangeSelector()
   }
   p
+}
+
+#' Extract ERDDAP statistics from polygon by year-month
+#'
+#' Extract satellite data in an ERDDAP dataset from national marine sanctuary polygon for a given year and month.
+#'
+#' @param sanctuary_code sanctuary code based on prefix to \href{https://sanctuaries.noaa.gov/library/imast_gis.html}{Sanctuary GIS files}, e.g. \code{"cinms"} for Channel Islands Marine Sanctuary
+#' @param erddap_id dataset ID of ERDDAP dataset (see \href{https://coastwatch.pfeg.noaa.gov/erddap/index.html}{coastwatch.pfeg.noaa.gov/erddap}), e.g. \code{"jplMURSST41mday"} for Multi-scale Ultra-high Resolution SST Analysis
+#' @param erddap_fld variable of ERDDAP dataset to extract, e.g. \code{"sst"}
+#' @param year 4-digit year
+#' @param month integer month (1-12)
+#' @param stats statistics to be generated for the data set \code{c("mean", "sd")}, to calculate mean and standard deviation)
+#'
+#' @return a list of values by statistic
+#'
+#' @export
+#' @import glue sf magrittr dplyr here rerddap
+#'
+#' @examples
+# This function gets the polygons for a National Marine Sanctuary
+ply2erddap <- function (sanctuary_code, erddap_id, erddap_fld, year, month, stats) {
+
+  # check inputs
+  stopifnot(all(is.numeric(year), is.numeric(month)))
+
+  # Get the polygons for the sanctuary.
+  sanctuary_ply <-   sf::as_Spatial(sf::st_union(nms4r::get_nms_polygons(sanctuary_code)))
+
+  # set the x and y limits of the raster to be pulled based upon the sanctuary polygons
+  bb <- sf::st_bbox(sanctuary_ply)
+
+  # TODO: deal with wrapping around dateline
+  # https://github.com/rstudio/leaflet/issues/225#issuecomment-347721709
+
+  # The dates to be considered (note that dates are handled differently when pulling different datasets)
+  m_beg   <- lubridate::ymd(glue::glue("{year}-{month}-01"))
+  m_end   <- m_beg + lubridate::days(lubridate::days_in_month(m_beg)) - lubridate::days(1)
+
+  # pull data from errdap server, with the process handled differently based upon the dataset - the value of erddap_id
+  # (as the datasets are not structured identically)
+
+  # set desired date range
+  m_dates <- c(m_beg, m_end)
+
+  nc <- try(
+    rerddap::griddap(
+      rerddap::info(erddap_id),
+      url = "https://coastwatch.pfeg.noaa.gov/erddap/",
+      time = m_dates,
+      #time = c("2013-01-01", "2013-06-01"),
+      #time = "2013-04-01",
+      latitude = c(bb$ymin, bb$ymax), longitude = c(bb$xmax, bb$xmin),
+      fields = erddap_fld, fmt = 'nc'))
+  if (class(nc) == "try-error"){
+    stats_na <- setNames(rep(NA, length(stats)), stats) %>% as.list()
+    return(stats_na)
+  }
+
+  if (erddap_id == "nesdisVHNSQchlaMonthly") { # pulling monthly chlorophyll data
+    # TODO: delete this chunk since this dataset now seems gone or renamed?, per https://coastwatch.pfeg.noaa.gov/erddap/griddap/nesdisVHNSQchlaMonthly.graph
+    ylim <- range(nc$data$lat, na.rm = TRUE)
+    xlim <- range(nc$data$lon, na.rm = TRUE)
+    ext <- raster::extent(xlim[1], xlim[2], ylim[1], ylim[2])
+
+    #create raster
+    d <- dplyr::arrange(nc$data, desc(nc$data$lat), nc$data$lon)
+    r <- raster::raster(nrows = length(unique(nc$data$lat)), ncols = length(unique(nc$data$lon)),
+                        ext = ext, vals = d[,erddap_fld])
+  } else { # if errdap_id calls any other dataset, stop everything as who knows how this other dataset is structured
+    # stop("Error in erddap_id: this function only currently knows how to handle the datasets jplMURSST41mday and nesdisVHNSQchlaMonthly")
+    r <- raster::raster(nc$summary$filename)
+  }
+
+  # The following get_stat function extracts a statistical value (eg. mean or standard deviation) from the raster
+  # cells remaining after being overlaid with the sanctuary polygons
+
+  get_stat <- function(stat){
+    fxn <- get(stat)
+    raster::extract(
+      r, sanctuary_ply, layer = 1,
+      method = "simple", na.rm=TRUE, fun = fxn)
+  }
+
+  # Let's run the function get_stat for every statistic asked for by the parameter value stats - this is the overall function output
+  sapply(stats, get_stat)
 }
